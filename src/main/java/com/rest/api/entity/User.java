c632f258c1c7928782493f1f40b73ae82841edc4
dud0701 <dud0701@naver.com>
2019-08-21 17:52:14 +0900

    init commit

---
+++ src/main/java/com/rest/api/entity/User.java
@@ -0,0 +1,23 @@
+package com.rest.api.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Builder
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="user")
+public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long msrl;
+    @Column(nullable=false, unique=true, length=30)
+    private String uid;
+    @Column(nullable = false, length = 100)
+    private String name;
+
+}
---
+++ src/main/java/com/rest/api/model/response/ListResult.java
@@ -0,0 +1,12 @@
+package com.rest.api.model.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+public class ListResult<T> extends CommonResult {
+    private List<T> list;
+}
